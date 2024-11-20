5c255b067e894d0cbd6a670de0cc45d2ab962752
Stefan Sjöberg <bird.nerd@outlook.com>
2024-11-20 12:31:53 +0100

    Add project files.

---
+++ ExerciseTask01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+List<string> ShoppingList = new List<string>();
+
+Console.WriteLine("Välkommen till din shopping/hungrig -lista!");
+Console.WriteLine("Skriv in produkter du vill lägga till som gör dig hungrig. Du kan skriva mätt när du vill avsluta och se listan. \n");
+
+while (true)
+{
+    Console.Write("Skriv in varor som kurrar i magen:");
+    string shoppingItem = Console.ReadLine();
+
+    // Om användaren skriver 'klar', avsluta inmatningen
+    if (shoppingItem.ToLower() == "mätt" || shoppingItem.ToLower() == "klar")
+    {
+        Console.WriteLine("\n");
+        break;
+    }
+
+    ShoppingList.Add(shoppingItem);
+}
+
+
+Console.WriteLine("Shopping/Hungrig -lista som du har skrivit:");
+foreach (var Shopping in ShoppingList)
+{
+    Console.WriteLine("- " + Shopping);
+}
+
+Console.ReadKey();
+
+/* 50/50 på Chatgpt-kod */
